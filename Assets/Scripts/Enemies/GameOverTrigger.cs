cf9257f2cd14af7ab1f0fe6bb76e8693167a10aa
gerodrus <gerodrus@gmail.com>
2020-04-18 19:24:44 +0300

    Added GameOverTrigger

---
+++ Assets/Scripts/Enemies/GameOverTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GameOverTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log($"GameOverTrigger.OnCollisionEnter2D other.gameObject={other.gameObject.name}");
+        var isPlayer = other.gameObject.GetComponent<Movement>() != null;
+        if (isPlayer)
+        {
+            Debug.LogError("GameOver");
+        }
+    }
+}
