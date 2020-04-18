71e95d706d801149e2fc15f24efd99581d3d8b37
gerodrus <gerodrus@gmail.com>
2020-04-18 17:53:39 +0300

    Added URP package and patcher for him

---
+++ Assets/Scripts/Editor/ShadowCaster2DPatcher.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public static class ShadowCaster2DPatcher
+{
+    static ShadowCaster2DPatcher()
+    {
+        var path = Application.dataPath.Replace('\\', '/');
+        path = path.Substring(0, path.Length - "Assets".Length);
+        path += "Library/PackageCache/com.unity.render-pipelines.universal@7.3.1/Runtime/2D/ShadowCaster2D.cs";
+
+        if (File.Exists(path))
+        {
+            var text1 = File.ReadAllText(Application.dataPath + "/Scripts/Editor/ShadowCaster2D.cs.txt");
+            var text2 = File.ReadAllText(path);
+            if (text1 != text2)
+            {
+                File.WriteAllText(path, text1);
+            }
+        }
+
+        AssetDatabase.Refresh();
+    }
+}
