eec117b530607963ec95e739c7180cd8271873da
gerodrus <gerodrus@gmail.com>
2020-04-18 20:16:54 +0300

    Added HomingMissileEnemy.calmForce and HomingMissileEnemy.calmMaxVelocity

--- Assets/Scripts/Enemies/HomingMissileEnemy.cs
+++ Assets/Scripts/Enemies/HomingMissileEnemy.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Collider2D))]
@@ -7,28 +8,42 @@ public class HomingMissileEnemy : MonoBehaviour
     public Transform target;
     public float force;
     public float maxVelocity;
+    public float calmForce;
+    public float calmMaxVelocity;
 
+    private Vector3 _startPosition;
     private Rigidbody2D _rigidbody2D;
     private Collider2D _collider2D;
 
     private void Awake()
     {
+        _startPosition = transform.localPosition;
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _collider2D = GetComponent<Collider2D>();
         // target = target != null ? target : FindObjectOfType<Movement>().transform;
     }
 
+    private void Start()
+    {
+        _rigidbody2D.AddForce(Vector2.right * calmForce);
+    }
+
     private void Update()
     {
-        if (target == null) return;
+        var hasTarget = target != null;
+        var vector = hasTarget
+            ? target.position - transform.position
+            : _startPosition - transform.localPosition;
 
-        var vector = target.position - transform.position;
+        var f = hasTarget ? force : calmForce;
         vector.Normalize();
-        _rigidbody2D.AddForce(vector * force);
+        _rigidbody2D.AddForce(vector * f);
         var currentSqrVelocity = _rigidbody2D.velocity.sqrMagnitude;
-        if (maxVelocity * maxVelocity < currentSqrVelocity)
+
+        var mv = hasTarget ? maxVelocity : calmMaxVelocity;
+        if (mv * mv < currentSqrVelocity)
         {
-            _rigidbody2D.velocity = _rigidbody2D.velocity.normalized * maxVelocity;
+            _rigidbody2D.velocity = _rigidbody2D.velocity.normalized * mv;
         }
     }
 }
