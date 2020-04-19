488b0e74f255b16617e65ae07c4593daf49a0563
gerodrus <gerodrus@gmail.com>
2020-04-19 10:27:05 +0300

    Added PauseMenu

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject root;
+
+    private bool _isPause = true;
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        root.SetActive(true);
+        _isPause = true;
+    }
+
+    public void ResumeGame()
+    {
+        root.SetActive(false);
+        Time.timeScale = 1;
+        _isPause = false;
+    }
+
+    private void Awake()
+    {
+        ResumeGame();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            if (_isPause)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+}
