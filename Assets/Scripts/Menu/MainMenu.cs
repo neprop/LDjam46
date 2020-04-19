0f5c15ef350494a151c160dac254c71e4fc40658
gerodrus <gerodrus@gmail.com>
2020-04-19 10:16:52 +0300

    Added MainMenu

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button exitButton;
+
+    public void Awake()
+    {
+#if UNITY_WEBGL
+        exitButton.gameObject.SetActive(false);
+#endif
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
