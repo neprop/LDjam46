f753eccbaba25375233a9d4f7d31cdf5d2f32a0f
gerodrus <gerodrus@gmail.com>
2020-04-19 09:36:57 +0300

    Added LevelCompleteTrigger

---
+++ Assets/Scripts/LevelCompleteTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelCompleteTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Debug.Log($"GameOverTrigger.OnCollisionEnter2D other.gameObject={other.gameObject.name}");
+        var isPlayer = other.gameObject.GetComponent<Movement>() != null;
+        if (isPlayer)
+        {
+            var activeScene = SceneManager.GetActiveScene();
+            Debug.Log($"Level#{activeScene.buildIndex} complete. Trying to load next scene. (Count={SceneManager.sceneCountInBuildSettings})");
+            SceneManager.LoadScene(activeScene.buildIndex + 1);
+        }
+    }
+}
